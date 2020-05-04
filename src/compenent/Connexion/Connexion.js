7f5a5a1363c1c7813c977000463215068ca0962c
Rifi Ismail <ismail.elydi@gmail.com>
2020-05-04 17:49:29 +0200

    Connexion functionnality ok

--- src/compenent/Connexion/Connexion.js
+++ src/compenent/Connexion/Connexion.js
@@ -1,10 +1,43 @@
 import React, { Component } from 'react'
+import { Redirect } from 'react-router-dom'
 
 export default class Connexion extends Component {
+    state={
+        pseudo:'',
+        goToChat:false,
+    }
+    handleChange = (e)=> {
+        this.setState({
+            pseudo:e.target.value
+        })
+    }
+    redirect=(e)=> {
+        const {pseudo}= this.state
+        e.preventDefault()
+        if(pseudo.trim()!== ''){
+            this.setState({
+                goToChat:true
+            })
+        }
+        else {
+            this.setState({
+                badName: `Veuillez insérer un pseudo.`
+            })
+        }
+    }
     render() {
+        const {pseudo,goToChat,badName}=this.state
+        if(goToChat){
+        return <Redirect push to={`/chat/${pseudo}`} />
+        }
         return (
             <div>
                 <h1>Connexion</h1>
+                <form onSubmit={this.redirect}>
+                    <input onChange={this.handleChange}/>
+                    <button type='submit'>Connexion</button>
+                    {badName ? <p>{badName}</p> : null}
+                </form>
             </div>
         )
     }
