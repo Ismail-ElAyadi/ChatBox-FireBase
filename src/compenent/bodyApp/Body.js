d5b22c3be3620ba239a0134df53e31543547fea1
Rifi Ismail <ismail.elydi@gmail.com>
2020-05-04 18:39:53 +0200

    The message is sended by the form

--- src/compenent/bodyApp/Body.js
+++ src/compenent/bodyApp/Body.js
@@ -3,14 +3,31 @@ import Chatbox from './Chatbox'
 
 export default class Body extends Component {
     state={
-        pseudo:null,
+        pseudo:this.props.match.params.pseudo,
         messages:{}
     }
+    sendMessage=(msg)=>{
+        const {messages,pseudo}=this.state
+        const allMessages = {...messages}
+        allMessages[Date.now()] = msg
+        this.setState({
+            messages:allMessages
+        })
+    }
     render() {
+        const {pseudo,messages}=this.state
+        //const chatMessages =
+
         return (
             <section>
-                <h1>ChatBox</h1>
-                <Chatbox/>
+                <h1>Welcome {pseudo}</h1>
+                <div>
+
+                </div>
+                <Chatbox
+                sendMessage =   {this.sendMessage}
+                pseudo      =   {pseudo}
+                />
             </section>
         )
     }
--- src/compenent/bodyApp/Chatbox.js
+++ src/compenent/bodyApp/Chatbox.js
@@ -9,17 +9,36 @@ handleMessage = (e)=> {
         currentMessage:e.target.value
     })
 }
+sendInfo=(e)=> {
+    const {currentMessage} = this.state
+    e.preventDefault()
+    if(currentMessage.trim() !== '')
+    {
+        this.props.sendMessage(currentMessage)
+        this.setState({
+        currentMessage:'',
+        messageEmpty:false
+        })
+    }
+    else
+    {
+        this.setState({
+            messageEmpty:'No message to send :('
+        })
+    }
 
+}
     render() {
-        const {currentMessage} = this.state
+        const {currentMessage,messageEmpty} = this.state
         return (
             <Fragment>
                 <div>
                     AllMeSsages
                 </div>
-                <form onSubmit={this.sendMessage}>
+                <form onSubmit={this.sendInfo}>
                 <textarea value={currentMessage} onChange={this.handleMessage}/>
                 <button type='submit'>Send</button>
+                {messageEmpty ? <p>{messageEmpty}</p>:null}
                 </form>
             </Fragment>
         )
