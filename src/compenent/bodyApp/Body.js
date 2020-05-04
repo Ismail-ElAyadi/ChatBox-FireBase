20eaa971da496bea15b5745e18213f41213c5b00
Rifi Ismail <ismail.elydi@gmail.com>
2020-05-04 19:26:34 +0200

    ChatBox Working , with no style and no Db

--- src/compenent/bodyApp/Body.js
+++ src/compenent/bodyApp/Body.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Chatbox from './Chatbox'
+import Message from './Message'
 
 export default class Body extends Component {
     state={
@@ -9,20 +10,37 @@ export default class Body extends Component {
     sendMessage=(msg)=>{
         const {messages,pseudo}=this.state
         const allMessages = {...messages}
-        allMessages[Date.now()] = msg
+        const date = new Date()
+        //msg.date= new Date().toLocaleDateString()
+        allMessages[Date.now()] = {
+            pseudo,
+            message:msg,
+            date:   `${date.getDate()}-${date.getMonth()+1}-${date.getFullYear()}`,
+            time:   `${date.getHours()}:${date.getMinutes()}`
+        }
         this.setState({
             messages:allMessages
         })
     }
     render() {
         const {pseudo,messages}=this.state
-        //const chatMessages =
+        const chatMessages = Object.keys(messages).map(e => {
+            const msg = messages[e]
+            return(
+                <Message
+                pseudo={msg.pseudo}
+                message={msg.message}
+                date={msg.date}
+                time={msg.time}
+                />
+            )
+        })
 
         return (
             <section>
                 <h1>Welcome {pseudo}</h1>
                 <div>
-
+                {chatMessages}
                 </div>
                 <Chatbox
                 sendMessage =   {this.sendMessage}
--- src/compenent/bodyApp/Message.js
+++ src/compenent/bodyApp/Message.js
@@ -1,10 +1,15 @@
 import React, { Component } from 'react'
 
 export default class Message extends Component {
+
     render() {
+        const {pseudo,message,date,time}=this.props
         return (
             <div>
-
+                {pseudo}
+                {message}
+                {date}
+                {time}
             </div>
         )
     }
