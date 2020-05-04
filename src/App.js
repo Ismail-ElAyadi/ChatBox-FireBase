6d2cff44722975ee6964348b8f606f445e712cf5
Rifi Ismail <ismail.elydi@gmail.com>
2020-05-04 18:15:16 +0200

    Updates Routes

--- src/App.js
+++ src/App.js
@@ -4,19 +4,19 @@ import Footer from './compenent/Footer/Footer'
 import Body from './compenent/bodyApp/Body'
 import Connexion from './compenent/Connexion/Connexion'
 import './assets/css/App.css'
-import { BrowserRouter, Switch, Route } from 'react-router-dom'
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
 import NotFound from './compenent/NotFound'
 export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
-      <Header/>
-      <Switch>
-        <Route exact path='/'> <Connexion/> </Route>
-        <Route path='/chat/:pseudo'> <Body/></Route>
-        <Route><NotFound/></Route>
-      </Switch>
-      <Footer/>
+        <Header/>
+          <Switch>
+                      <Route exact path='/' component={Connexion} />
+                      <Route path='/chat/:pseudo' component={Body}/>
+                      <Route path='*' component={NotFound}/>
+          </Switch>
+        <Footer/>
       </BrowserRouter>
     )
   }
